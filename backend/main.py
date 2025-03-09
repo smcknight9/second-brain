37594eb702ac4dcf531d466c1df498761c59fe16
Shawna McKnight <smcknight9@gmail.com>
2025-03-08 21:17:19 -0800

    fix db naming

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from backend.models import Base
 from backend.models import JournalEntry
 from backend.api.endpoints import router
 
-DATABASE_URL = "postgresql://user:password@db:5432/secondbrain"
+DATABASE_URL = "postgresql://user:password@secondbrain_db:5432/secondbrain_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
